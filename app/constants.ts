55907486ce26b9fc39c887fd537d51ed484564dd
fom78 <fom78a@gmail.com>
2021-12-21 11:30:30 -0300

    inicial

---
+++ app/constants.ts
@@ -0,0 +1,20 @@
+const phone: string =`5491141414141`;
+export const INFORMATION = {
+  avatar: "/assets/avatar.jpg",
+  banner: "/assets/banner.jpg",
+  title: "Aromas del Corazon",
+  description: "Aromas que vienen desde el corazon para tus ambientes",
+  phone: `${phone}`,
+  sheet: `https://docs.google.com/spreadsheets/d/e/2PACX-1vSwlmbAX7LzitQziUx28qz1Iq1O66-sroir0UI6bC2K4GOh_wcYE42zvKaHbEwIsbN3SquSmOgH28Y8/pub?output=csv`,
+  color: `teal`,
+  social: [
+    {
+      name: "instagram",
+      url: `https://instagram.com/goncy.js`,
+    },
+    {
+      name: "whatsapp",
+      url: `https://wa.me/${phone}`,
+    },
+  ],
+};
